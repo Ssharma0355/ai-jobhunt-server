f6e857a306b1fe650e6edd6bb824d2f1dab956e7
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-03-03 14:51:17 +0530

    Data base Connected to MongoDB Successfully

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+require("dotenv").config()
+const connectToDatabse = require("./src/config/database")
 const app = require("./src/app");
 PORT = 8000
+
+connectToDatabse();
 
 app.listen(PORT, ()=>{
     console.log("Server Running on port",PORT)
